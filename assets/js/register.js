1e23145fca4963c9abd5e213275b3c7bdf4eb4e6
Rodhi <Rodhi@gmail.com>
2022-10-19 19:37:10 +0700

    add loading button

--- assets/js/register.js
+++ assets/js/register.js
@@ -2,16 +2,33 @@ let getForm = document.querySelector("#registration")
 let getInputanFullname = document.querySelector("#fullname")
 let getInputanEmail = document.querySelector("#email")
 let getInputanPassword = document.querySelector("#password")
+let getButtonLoading = document.querySelector("#loading")
+let getButtonNotLoading = document.querySelector("#not-loading")
+
+getButtonNotLoading.style.display="block"
+getButtonLoading.style.display="none"
+
+
+
+let loading = () => {
+    getButtonLoading.style.display="block"
+    getButtonNotLoading.style.display="none"
+}
+
+
 
 const url = "https://634d3cccacb391d34a979eb9.mockapi.io/api/konsul/users"
 
 getForm.addEventListener("submit", e => {
+   
     e.preventDefault()
         let user = {
             userFullname : getInputanFullname.value,
             userEmail : getInputanEmail.value,
             userPassword : getInputanPassword.value
         }
+
+        loading()
 
          const cekApi = async() => {
             try {   
@@ -27,8 +44,10 @@ getForm.addEventListener("submit", e => {
                             password: user.userPassword
                     }),
                 })
+
                 let dataApi = await getDataApi.json()
                 console.log(dataApi);
+
                 Swal.fire(
                     'Register berhasil!',
                     'Silahkan kamu bisa login',
