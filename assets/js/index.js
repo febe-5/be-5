55fb22059de450f9a2787c95b085668f55e74feb
Abilsabili <abilsabili50@gmail.com>
2022-10-20 12:01:44 +0700

    benerin struktur js sama nambahin autentikasi

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,4 @@
 const resultCarousel = document.getElementById("result-carousel");
-const btnLogin = document.getElementById("btn-login");
-const btnLogout = document.getElementById("btn-logout");
-
-btnLogout.addEventListener("click", () => {
-	localStorage.clear();
-	window.location.href = login.html;
-});
 
 const fetchApi = async () => {
 	resultCarousel.innerHTML = "";
@@ -31,32 +24,6 @@ const fetchApi = async () => {
 		console.log(error.message);
 	}
 };
-
-const fetchUser = async () => {
-	const id = localStorage.getItem("id");
-	const userById = fetch(
-		"https://634d3cccacb391d34a979eb9.mockapi.io/api/konsul/users/"
-	)
-		.then((response) => response.json())
-		.then((response) => {
-			const user = response.find((element) => element.id == id);
-			if (!user) {
-				btnLogin.style.display = "block";
-				btnLogout.style.display = "none";
-			} else {
-				if (!user.login) {
-					btnLogin.style.display = "block";
-					btnLogout.style.display = "none";
-				} else {
-					btnLogin.style.display = "none";
-					btnLogout.style.display = "block";
-				}
-			}
-		})
-		.catch((error) => console.log(error.message));
-};
-
-fetchUser();
 
 fetchApi();
 
--- assets/js/list-psikolog.js
+++ assets/js/list-psikolog.js
@@ -1,6 +1,4 @@
 let listPsikolog = document.querySelector("#content");
-const btnLogin = document.getElementById("btn-login");
-const btnLogout = document.getElementById("btn-logout");
 const url = "https://634d3cccacb391d34a979eb9.mockapi.io/api/konsul/psikolog";
 
 getDataPsikolog = async () => {
@@ -31,34 +29,3 @@ getDataPsikolog();
 function addDetail(id) {
 	console.log(id);
 }
-
-btnLogout.addEventListener("click", () => {
-	localStorage.clear();
-	window.location.href = login.html;
-});
-
-const fetchUser = async () => {
-	const id = localStorage.getItem("id");
-	const userById = fetch(
-		"https://634d3cccacb391d34a979eb9.mockapi.io/api/konsul/users/"
-	)
-		.then((response) => response.json())
-		.then((response) => {
-			const user = response.find((element) => element.id == id);
-			if (!user) {
-				btnLogin.style.display = "block";
-				btnLogout.style.display = "none";
-			} else {
-				if (!user.login) {
-					btnLogin.style.display = "block";
-					btnLogout.style.display = "none";
-				} else {
-					btnLogin.style.display = "none";
-					btnLogout.style.display = "block";
-				}
-			}
-		})
-		.catch((error) => console.log(error.message));
-};
-
-fetchUser();
