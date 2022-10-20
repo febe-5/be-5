edbcae55f0d22f4b61219f45e9358d19cfb4b045
Abilsabili <abilsabili50@gmail.com>
2022-10-20 10:36:35 +0700

    fix login per page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,11 @@
 const resultCarousel = document.getElementById("result-carousel");
+const btnLogin = document.getElementById("btn-login");
+const btnLogout = document.getElementById("btn-logout");
+
+btnLogout.addEventListener("click", () => {
+	localStorage.clear();
+	window.location.href = login.html;
+});
 
 const fetchApi = async () => {
 	resultCarousel.innerHTML = "";
@@ -25,6 +32,32 @@ const fetchApi = async () => {
 	}
 };
 
+const fetchUser = async () => {
+	const id = localStorage.getItem("id");
+	const userById = fetch(
+		"https://634d3cccacb391d34a979eb9.mockapi.io/api/konsul/users/"
+	)
+		.then((response) => response.json())
+		.then((response) => {
+			const user = response.find((element) => element.id == id);
+			if (!user) {
+				btnLogin.style.display = "block";
+				btnLogout.style.display = "none";
+			} else {
+				if (!user.login) {
+					btnLogin.style.display = "block";
+					btnLogout.style.display = "none";
+				} else {
+					btnLogin.style.display = "none";
+					btnLogout.style.display = "block";
+				}
+			}
+		})
+		.catch((error) => console.log(error.message));
+};
+
+fetchUser();
+
 fetchApi();
 
 function tampilData(data) {
@@ -32,9 +65,9 @@ function tampilData(data) {
   <div class="row bg-light align-items-center p-5">
     <div class="col-sm-7">
       <img
-        src="assets/images/psikolog.svg"
+        src="assets/images/user.png"
         width="150px"
-        class="img-thumbnail rounded-circle"
+        class="img-thumbnail"
         alt=""
       />
     </div>
@@ -42,7 +75,7 @@ function tampilData(data) {
       <h4>${data.name}</h4>
       <p>Pengalaman : <span>${data.pengalaman} Tahun</span></p>
       <div>
-        <a href="list-psikolog.html?id=${data.id}" class="btn btn-primary" type="button">
+        <a href="detail-psikolog.html?id=${data.id}" class="btn btn-primary" type="button">
           Konsultasi Sekarang
         </a>
       </div>
@@ -56,9 +89,9 @@ function tampilDataActive(data) {
   <div class="row bg-light align-items-center p-5">
     <div class="col-sm-7">
       <img
-        src="assets/images/psikolog.svg"
+        src="assets/images/user.png"
         width="150px"
-        class="img-thumbnail rounded-circle"
+        class="img-thumbnail"
         alt=""
       />
     </div>
@@ -66,7 +99,7 @@ function tampilDataActive(data) {
       <h4>${data.name}</h4>
       <p>Pengalaman : <span>${data.pengalaman} Tahun</span></p>
       <div>
-        <a href="list-psikolog.html?id=${data.id}" class="btn btn-primary" type="button">
+        <a href="detail-psikolog.html?id=${data.id}" class="btn btn-primary" type="button">
           Konsultasi Sekarang
         </a>
       </div>
--- assets/js/list-psikolog.js
+++ assets/js/list-psikolog.js
@@ -1,13 +1,15 @@
 let listPsikolog = document.querySelector("#content");
+const btnLogin = document.getElementById("btn-login");
+const btnLogout = document.getElementById("btn-logout");
 const url = "https://634d3cccacb391d34a979eb9.mockapi.io/api/konsul/psikolog";
 
 getDataPsikolog = async () => {
-    const response = await fetch(url);
-    const psikolog = await response.json();
-    console.log(psikolog);
+	const response = await fetch(url);
+	const psikolog = await response.json();
+	console.log(psikolog);
 
-    psikolog.forEach((list) => {
-        listPsikolog.innerHTML += `
+	psikolog.forEach((list) => {
+		listPsikolog.innerHTML += `
         <div class="col-lg-3 col-sm-6 mb-3" id="content">
             <div class="card">
                 <div class="card-body">
@@ -15,18 +17,48 @@ getDataPsikolog = async () => {
                         <img src="assets/images/user.png" width="100" height="100" class="rounded-circle" alt="${list.name}">
                         <h5 class="mt-3 mb-1">${list.name}</h5>
                         <p class="m-2">${list.specialist}</p>
-                        <a href="#" class="btn btn-sm btn-outline-primary" onClick="addDetail(${list.id})">Pilih</a>
+                        <a href="detail-psikolog.html?id=${list.id}" class="btn btn-sm btn-outline-primary" onClick="addDetail(${list.id})">Pilih</a>
                     </div>
                 </div>
             </div>
         </div>
         `;
-    });
-}
+	});
+};
 
 getDataPsikolog();
 
-
 function addDetail(id) {
-    console.log(id);
+	console.log(id);
 }
+
+btnLogout.addEventListener("click", () => {
+	localStorage.clear();
+	window.location.href = login.html;
+});
+
+const fetchUser = async () => {
+	const id = localStorage.getItem("id");
+	const userById = fetch(
+		"https://634d3cccacb391d34a979eb9.mockapi.io/api/konsul/users/"
+	)
+		.then((response) => response.json())
+		.then((response) => {
+			const user = response.find((element) => element.id == id);
+			if (!user) {
+				btnLogin.style.display = "block";
+				btnLogout.style.display = "none";
+			} else {
+				if (!user.login) {
+					btnLogin.style.display = "block";
+					btnLogout.style.display = "none";
+				} else {
+					btnLogin.style.display = "none";
+					btnLogout.style.display = "block";
+				}
+			}
+		})
+		.catch((error) => console.log(error.message));
+};
+
+fetchUser();
