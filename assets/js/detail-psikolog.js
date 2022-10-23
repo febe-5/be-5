95f94f43304370494e22cc772030ff2ec8d3ea67
Denna Mandela <denna.mandela13@gmail.com>
2022-10-23 11:21:14 +0700

    update detail fix

--- assets/js/detail-psikolog.js
+++ assets/js/detail-psikolog.js
@@ -16,7 +16,6 @@ const getDataPsikolog = async () => {
 	const response = await fetch(URL);
 	psikolog = await response.json();
 
-    psikolog.forEach(e => {
         detail.innerHTML += `<div class="col-md-5 mb-5">
         <div class="row">
             <img src="https://www.w3schools.com/w3css/img_avatar3.png" alt="Hero logo"
@@ -25,25 +24,23 @@ const getDataPsikolog = async () => {
         <div class="row text-center mt-4 mb-3">
             <div class="col-lg-6 border-lg-end">
                 <p class="h3">Pengalaman</p>
-                <p class="fs-5">5 Tahun</p>
+                <p class="fs-5">${psikolog.pengalaman} Tahun</p>
             </div>
             <div class="col-lg-6">
                 <p class="h3">Harga</p>
-                <p class="fs-5">Rp. 200.000</p>
+                <p class="fs-5">Rp. ${psikolog.harga}</p>
             </div>
         </div>
         <div class="row">
             <div class="d-grid gap-2 col-md-6 mx-auto">
-                <button type="submit" class="btn btn-primary">
-                    Konsultasi dengan ${e.name}
-                </button>
+                <a href="pembayaran.html?id=${psikolog.id}" class="btn btn-primary">Konsultasi dengan ${psikolog.name}</a>
             </div>
         </div>
     </div>
     <div class="col-md-7" id="detail">
         <div class="row border-bottom pb-3">
-            <h1>${e.name}</h1>
-            <h4>${e.specialist}</h4>
+            <h1>${psikolog.name}</h1>
+            <h4>${psikolog.specialist}</h4>
         </div>
         <div class="row border-bottom py-3">
             <h6>Melayani Via : zoom</h6>
@@ -52,7 +49,7 @@ const getDataPsikolog = async () => {
             <h6>Jadwal Praktik : Senin & Jum'at</h6>
             <h6 class="mt-3 mb-4">Profil</h6>
             <p>
-                ${e.Description}
+                ${psikolog.Description}
             </p>
         </div>
         </div>`
