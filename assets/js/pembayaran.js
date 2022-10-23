2d03d4ab6652bfa7d3455f3cc47527670d6249ce
Denna Mandela <denna.mandela13@gmail.com>
2022-10-23 12:13:06 +0700

    nambah alert berisi link zoom

--- assets/js/pembayaran.js
+++ assets/js/pembayaran.js
@@ -31,3 +31,10 @@ fetch("https://634d3cccacb391d34a979eb9.mockapi.io/api/konsul/psikolog/" + id)
 		harga.innerHTML = response.harga;
 	})
 	.catch((error) => console.log(error));
+
+function bayar() {
+	let linkZoom = confirm ("Pembayaran berhasil dilakukan, Silahkan klik link disamping.");
+	if (linkZoom) {
+		window.location="https://us04web.zoom.us/j/2515990968?pwd=ZjBFTmFaSnhZbklCYTZ1MUxmK3hRZz09";
+	}
+}
